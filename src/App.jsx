fd4dc7edc637e459222070d1c64d1eb960f465c0
yuvvinn <iyubin284@gmail.com>
2025-11-25 20:40:43 +0900

    feat: 헤더 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AuthPage from './Pages/AuthPage'
-
+import Header from './Components/Header'
 function App() {
   return (
     <BrowserRouter>
-    <AuthPage />
-      {/* <Header/> */}
+    {/* <AuthPage /> */}
+      <Header/>
       {/* <SignupPage />
       {/* <AuthLayout /> */}
       {/* <Routes>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,44 @@
-// import styles from "../styles/Header.module.css";
+import styles from "../Styles/Header.module.css";
+import headerlogo from '../assets/HeaderLogo.svg'
+import headersearch from '../assets/HeaderSearch.svg'
+import profileImage from '../assets/ProfileImage.svg'
+import { Link } from 'react-router-dom'
+
 
 function Header(){
   return(
-    <div className="Header">
-      <h1>요기가 header</h1>
+    <div className={styles.Header}>
+      <div className={styles.Headerlogo}>
+        <img src={headerlogo} alt="logo" />
+      </div>
+
+      <div className={styles.Headernav}>
+            <ul>
+                <li>
+                    <Link to="/about">소개</Link>
+                </li>
+                <li>
+                    <Link to="/coach">코치</Link>
+                </li>
+            </ul>
+        </div>
+
+        <div className={styles.Headersearch}>
+            <input type="text" placeholder="모임을 검색해 보세요!" />
+            <button>
+              <img src={headersearch} alt="search" />
+            </button>
+            
+        </div>
+
+        <div className={styles.Headerlogin}>
+          <span className={styles.HeaderStatus}>로그아웃 상태입니다</span>
+          <button className={styles.HeaderProfileButton}>
+            <img src={profileImage} alt="user profile" />
+          </button>
+        </div>
+        
+    
     </div>
   )
 }
