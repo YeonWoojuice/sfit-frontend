09ecbfae0ac02281a32765f3039cada84ba2580d
jjong0923 <90667780+jjong0923@users.noreply.github.com>
2025-11-25 22:00:19 +0900

    .

--- src/Pages/AuthPage.jsx
+++ src/Pages/AuthPage.jsx
@@ -1,24 +1,20 @@
-import AuthLayout  from "../Components/AuthLayout";
-import {useState} from "react";
+import AuthLayout from "../Components/AuthLayout";
+import { useState } from "react";
 import styles from "../styles/AuthPage.module.css";
-// import styles from "../styles/AuthPage.module.css";
 
-
-function AuthPage(){
-
+function AuthPage() {
   const [isLoginMode, setIsLoginMode] = useState(true);
 
-  const switchMode = () => setIsLoginMode(prev => !prev);
+  const switchMode = () => setIsLoginMode((prev) => !prev);
 
-
-  return( 
-  
-  <div className={`${styles.page} ${isLoginMode ? styles.loginMode : styles.signupMode}`}>
-    <AuthLayout
-      isLoginMode={isLoginMode}
-      onSwitch={switchMode}
-    />
-  </div>
-  )
+  return (
+    <div
+      className={`${styles.page} ${
+        isLoginMode ? styles.loginMode : styles.signupMode
+      }`}
+    >
+      <AuthLayout isLoginMode={isLoginMode} onSwitch={switchMode} />
+    </div>
+  );
 }
 export default AuthPage;
