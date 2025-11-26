cdd2a46da58ca1668d7bc6b4f052bdefe6ca8f58
yuvvinn <iyubin284@gmail.com>
2025-11-26 15:29:58 +0900

    헤더 CSS 스타일 수정 및 라우터 설정 변경
    - Header: 로고 클릭 시 메인 페이지 이동 기능 추가
    - Header: 프로필 버튼 및 로그아웃 텍스트 클릭 시 로그인 페이지 이동 기능 추가
    - App: AuthPage 라우트를 Layout 안으로 이동하여 모든 페이지에서 Header 고정 표시
    - Header CSS: 로고 중앙 정렬 스타일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<MainPage />} />
+          <Route path="auth" element={<AuthPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
