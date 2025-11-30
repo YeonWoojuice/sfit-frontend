53a0bcb806ed0445d4bb68562bc81387c33f69fc
jongmin <90667780+jjong0923@users.noreply.github.com>
2025-12-01 01:54:16 +0900

    Update MyPage.jsx

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,7 +1,7 @@
 import styles from "../styles/my/MyPage.module.css";
 import BadgeList from "../components/my/BadgeList";
 import MyClubList from "../components/my/MyClubList";
-import UpcomingMeetups from "../components/my/UpcomingMeetups";
+import UpcomingMeetups from "../components/my/UpComingMeetups";
 import UserHistory from "../components/my/UserHistory";
 import UserProfile from "../components/my/UserProfile";
 import { useState, useEffect } from "react";
