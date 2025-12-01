b05b0b902ce9231e207e3525d3505df0182ecd94
yuvvinn <iyubin284@gmail.com>
2025-12-02 00:24:16 +0900

    modified coach page

--- src/components/coach/CoachCard.jsx
+++ src/components/coach/CoachCard.jsx
@@ -2,18 +2,33 @@ import styles from "../../styles/coach/CoachCard.module.css";
 import chatIcon from "../../assets/chat.svg";
 import coachchat from "../../assets/coach/coachchat.svg";
 import coachcall from "../../assets/coach/coachcall.svg";
+import coachstar from "../../assets/coach/coachstar.svg";
 
 function CoachCard({ coach }) {
   return (
     <div className={styles.container}>
-      <div className={styles.profileImage}></div>
       <div className={styles.content}>
         <div className={styles.nameRow}>
           <div className={styles.nameSection}>
             <h3 className={styles.name}>{coach?.name || "고미경"}</h3>
-            <div className={styles.sportRow}>
-              <span className={styles.sportName}>{coach?.sport || "종목 명"}</span>
-              <span className={styles.yellowDot}></span>
+            <span className={styles.sportName}>{coach?.sport || "종목 명"}</span>
+            <span className={styles.yellowDot}></span>
+          </div>
+          <div className={styles.ratingSection}>
+            <img src={coachstar} alt="star" className={styles.star} />
+            <span className={styles.ratingValue}>
+              {coach?.rating || "4.86"}
+            </span>
+            <span className={styles.reviewCount}>
+              ({coach?.reviewCount || "247"})
+            </span>
+            <div className={styles.contactIcons}>
+              <button className={styles.iconBtn}>
+                <img src={coachchat} alt="chat" />
+              </button>
+              <button className={styles.iconBtn}>
+                <img src={coachcall} alt="call" className={styles.callIcon} />
+              </button>
             </div>
           </div>
         </div>
@@ -21,31 +36,13 @@ function CoachCard({ coach }) {
           <p>{coach?.description1 || "소통과 공감으로 함께 하는 시간~"}</p>
           <p>{coach?.description2 || "일정 조율 및 가격 제시"}</p>
         </div>
-        <div className={styles.ratingRow}>
-          <div className={styles.ratingSection}>
-            <span className={styles.star}>★</span>
-            <span className={styles.ratingValue}>
-              {coach?.rating || "4.86"}
-            </span>
-            <span className={styles.reviewCount}>
-              ({coach?.reviewCount || "247"})
-            </span>
-          </div>
-          <div className={styles.contactIcons}>
-            <button className={styles.iconBtn}>
-              <img src={coachcall} alt="call" />
-            </button>
-            <button className={styles.iconBtn}>
-              <img src={coachchat} alt="chat" />
-            </button>
-          </div>
-        </div>
         <div className={styles.tags}>
           <span className={styles.tag}>종목</span>
           <span className={styles.tag}>지역</span>
           <span className={styles.tag}>나이대</span>
         </div>
       </div>
+      <div className={styles.profileImage}></div>
     </div>
   );
 }
